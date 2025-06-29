6923edc60b49d89222ab928b198051e2628e69b1
Rustam Khanakhmedov <bateg@mail.ru>
2025-06-30 00:55:28 +0300

    F5

---
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Рустам Ханахмедов on 22.06.2025.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    var image: UIImage? {
+        didSet {
+            guard isViewLoaded else { return }
+            imageView.image = image
+        }
+    }
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imageView.image = image
+    }
+}
+
